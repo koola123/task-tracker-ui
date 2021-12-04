deb3e5116a4df277e1d98492aef75e6938a0b57c
koola123 <82808273+koola123@users.noreply.github.com>
2021-12-04 23:02:44 +0100

    Final

--- src/App.js
+++ src/App.js
@@ -1,11 +1,78 @@
+import Header from "./components/Header"
+import Notes from "./components/Notes"
+import { useState } from "react"
+import AddNote from "./components/AddNote"
 
 
-function App() {
+
+const App = () => {
+  const [showAddNote, setShowAddNote] = useState(false)
+  const [notes, setNotes] = useState(
+    [
+      {
+        id: 1,
+        text: "Book flight tickets",
+        day: "Dec 23rd at 2:30pm",
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: "Christmas shopping",
+        day: "Dec 20th at 4:30pm",
+        reminder: false,
+      },
+      {
+        id: 3,
+        text: "Hotel booking",
+        day: "Dec 20th at 11:00am",
+        reminder: true,
+      },
+      {
+        id: 4,
+        text: "Go to Hertz rental station",
+        day: "Dec 23rd at 2:30am",
+        reminder: true,
+      }
+    ]
+  )
+
+
+  // 😎 Add Note 🪄
+  const addNote = (note) => {
+    const id = Math.floor(Math.random() * 1000) + 1
+    const newNote = { id, ...note }
+    setNotes([...notes, newNote])
+  }
+
+  // 😎 Delete Note 🪄
+  const deleteNote = (id) => {
+    setNotes(notes.filter((note) => note.id !== id))
+  }
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setNotes(notes.map((note) =>
+      note.id === id ? {
+        ...note, reminder:
+          !note.reminder
+      } : note))
+  }
+
+
+
   return (
-    <div className="App">
-
-    </div>
+    <div className="container">
+      <Header onAdd={() => setShowAddNote(!showAddNote)} showAdd={showAddNote} />
+      {showAddNote && <AddNote onAdd={addNote} />}
+      {notes.length > 0 ? (<Notes notes={notes}
+        onDelete={deleteNote} onToggle={toggleReminder}
+      />
+      ) : ('Keine Notizen zum Anzeigen vorhanden.'
+      )}
+    </div >
   );
 }
+
+
 
 export default App;
